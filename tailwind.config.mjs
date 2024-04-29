e9e8f36e1c32302b3b8a40af82cf2dea500dfbb2
Luis <luiscvlh11@gmail.com>
2024-04-29 19:51:52 -0300

    feat: :sparkles: add sass support and base.scss file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#",
+                secondary: "#",
+                light: "#",
+                dark: "#",
+            },
+            fontFamily: {
+                // sans: [],
+            },
+        },
+    },
+    plugins: [],
+};
